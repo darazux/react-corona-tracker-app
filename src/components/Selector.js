1e59b5b7f70b5a95e240e4c7c4b7a37bc06e485b
Atsushi ITANI <darazu@uranus.dti.ne.jp>
2023-12-09 17:52:06 +0900

    * add Selector options from local file.

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,9 +1,20 @@
 // Selector.js
 
+import { useState } from 'react';
+import countriesJson from '../countries.json';
+
 const Selector = () => {
+  const [country, setCountry] = useState('');
   return (
     <div>
       <p>セレクター</p>
+      <select>
+        {countriesJson.map((country, index) => (
+          <option key={index} value={country.Slug}>
+            {country.Country}
+          </option>
+        ))}
+      </select>
     </div>
   );
 };
