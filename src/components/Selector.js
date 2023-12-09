ea07da48fb3c46a112a024e71481650f8a6c4077
Atsushi ITANI <darazu@uranus.dti.ne.jp>
2023-12-09 17:59:41 +0900

    * add fetching corona data function. * add button 'Get Data'.

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -5,16 +5,24 @@ import countriesJson from '../countries.json';
 
 const Selector = () => {
   const [country, setCountry] = useState('');
+  const getCountryData = () => {
+    fetch(
+      `https://proxy-server-node.vercel.app/corona-tracker-country-data?${country}`,
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
   return (
     <div>
       <p>セレクター</p>
-      <select>
+      <select onChange={(e) => setCountry(e.target.value)}>
         {countriesJson.map((country, index) => (
           <option key={index} value={country.Slug}>
             {country.Country}
           </option>
         ))}
       </select>
+      <button onClick={getCountryData}>Get Data</button>
     </div>
   );
 };
